23177ce64584c179a8d6a2b8c1b5d0b9f833a004
SeranaZ <kblitz71@gmail.com>
2024-09-11 12:06:08 -0400

    Revert "added a line to break cde"
    This reverts commit 4b785cc25a945ea969f55ad657497c7a94008beb.

--- game.py
+++ game.py
@@ -24,8 +24,6 @@ def main():
         else:
             print("Wrong!")
 
-        now its broken #added a line to break code
-
     print(f"Your final score is {score} out of {len(questions)}")
 
 if __name__ == "__main__":
